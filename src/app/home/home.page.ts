f2d952658fa45cb254a71b25f3aeec8cc43a62bd
Ishmeet sethi <sethi.ishmeet@gmail.com>
2021-05-10 22:48:54 -0400

    Updated home page

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  socials: SocialLinks[];
+  private baseLinks: SocialLinks[] = [
+    {
+      image: '../../assets/twitter.png',
+      link: 'https://twitter.com/IshmeetSing',
+    },
+    {
+      image: '../../assets/linkedin.png',
+      link: 'https://www.linkedin.com/in/ishmeetsinghsethi',
+    },
+  ];
 
   constructor() {}
 
+  ngOnInit(): void {
+    const prefersDark = window.matchMedia('(prefers-color-scheme: dark)');
+
+    this.toggleDarkTheme(prefersDark.matches);
+
+    // Listen for changes to the prefers-color-scheme media query
+    prefersDark.addListener((mediaQuery) =>
+      this.toggleDarkTheme(mediaQuery.matches)
+    );
+  }
+
+  toggleDarkTheme(isDark) {
+    if (isDark) {
+      this.socials = [
+        ...this.baseLinks,
+        {
+          image: '../../assets/github-white.png',
+          link: 'https://github.com/sethi-ishmeet',
+        },
+      ];
+    } else {
+      this.socials = [
+        ...this.baseLinks,
+        {
+          image: '../../assets/github.png',
+          link: 'https://github.com/sethi-ishmeet',
+        },
+      ];
+    }
+  }
+}
+
+interface SocialLinks {
+  link: string;
+  image: string;
 }
